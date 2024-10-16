fae813521b0b59b21c0c8bfa1f3e3f6c6b560ef5
Sam Dayo <samuelday2017@gmail.com>
2024-10-15 23:23:56 -0700

    finished part A, functional; need to ccheck speed

--- part_a/tasksys.cpp
+++ part_a/tasksys.cpp
@@ -1,6 +1,5 @@
 #include "tasksys.h"
 
-
 IRunnable::~IRunnable() {}
 
 ITaskSystem::ITaskSystem(int num_threads) {}
@@ -48,28 +47,81 @@ const char* TaskSystemParallelSpawn::name() {
     return "Parallel + Always Spawn";
 }
 
-TaskSystemParallelSpawn::TaskSystemParallelSpawn(int num_threads): ITaskSystem(num_threads) {
+TaskSystemParallelSpawn::TaskSystemParallelSpawn(int num_threads): ITaskSystem(num_threads), num_threads(num_threads){
     //
     // TODO: CS149 student implementations may decide to perform setup
     // operations (such as thread pool construction) here.
     // Implementations are free to add new class member variables
     // (requiring changes to tasksys.h).
     //
+
 }
 
 TaskSystemParallelSpawn::~TaskSystemParallelSpawn() {}
 
-void TaskSystemParallelSpawn::run(IRunnable* runnable, int num_total_tasks) {
 
+void TaskSystemParallelSpawn::threadingRun(IRunnable* runnable, int num_total_tasks, int start, int end){
+    for (int i = start; i < end; i++) {
+        runnable->runTask(i, num_total_tasks);
+    }
+}
+void TaskSystemParallelSpawn::run(IRunnable* runnable, int num_total_tasks) {
 
     //
     // TODO: CS149 students will modify the implementation of this
     // method in Part A.  The implementation provided below runs all
     // tasks sequentially on the calling thread.
     //
+    
+    /*we need to split the total work amongst the number of threads. 
+        The number of threads should match the num_threads.
+    */
 
-    for (int i = 0; i < num_total_tasks; i++) {
-        runnable->runTask(i, num_total_tasks);
+   /*
+    The code below yielded insufficient improvements.
+        Strategies for speed up: using non sequential static assignment
+   */
+    // int thread_tasks = (int) ceil(num_total_tasks / num_threads); 
+
+    // std::vector <std::thread> workers; //instantiate num_threads number of threads.
+
+    // for (int i = 0; i < num_threads; i++){
+    //     int starting_task = i*thread_tasks; // offset start for each thread
+
+    //     // deals with case where the number of total task is not perfectly divisible by the number of threads.
+    //     int end_task = ((starting_task + thread_tasks) < num_total_tasks)? starting_task + thread_tasks : num_total_tasks;
+
+    //     workers.emplace_back([runnable, starting_task, end_task](){
+    //         for (int i = starting_task; i < end_task; ++i) {
+    //             runnable->runTask(i, num_total_tasks);
+    //         }
+    //     });
+    // }
+
+    // for ( int i = 0; i < num_threads; ++i){
+    //     workers[i].join();
+    // }
+
+    // int thread_tasks = (int) ceil(num_total_tasks / num_threads); 
+
+    std::vector <std::thread> workers; //instantiate num_threads number of threads.
+    std::atomic<int> task_counter(0); 
+
+    auto runThread = [&task_counter, runnable, num_total_tasks, this](){
+        while(true){
+            // natch dynamic tasking (batch of 4)
+            int task = task_counter.fetch_add(1);
+            if (task >= num_total_tasks) break;
+            runnable->runTask(task, num_total_tasks);
+        }
+    };
+    
+    for(int i =0; i < num_threads; i++){
+        workers.emplace_back(runThread);
+    }
+
+    for ( int i = 0; i < num_threads; ++i){
+        workers[i].join();
     }
 }
 
